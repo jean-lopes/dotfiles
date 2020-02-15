07638ae7ec1faf5f77a4ec7354233ff9765debee
Jean Lopes <jean.lopes@hotmail.com.br>
2020-02-15 19:37:55 -0300

    䠲

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar --config=$HOME/.config/polybar/config mybar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --config=$HOME/.config/polybar/config --reload mybar &
+  done
+else
+  polybar --config=$HOME/.config/polybar/config --reload mybar &
+fi
 
 echo "Polybar launched..."
--- .omega/setup.sh
+++ .omega/setup.sh
@@ -1,3 +1,4 @@
 dir=$(pwd)
 cp -f $dir/etc/X11/xorg.conf.d/*.conf  /etc/X11/xorg.conf.d
 cp -f $dir/etc/acpi/events/*           /etc/acpi/events
+
